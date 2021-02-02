cd8a5c8471979a718a078aca7b879253398dc6b0
olukra <oleg.bn@gmail.com>
2021-02-01 23:34:48 -0800

    Initial commit

---
+++ touchdraw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  touchdraw
+//
+//  Created by Oleg on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
